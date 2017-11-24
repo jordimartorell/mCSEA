588c961c23c4067f882c026734d8ca24405d8473
jordimartorell <33520613+jordimartorell@users.noreply.github.com>
2017-11-24 06:58:41 -0500

    Update test_mCSEATest.R

--- inst/unitTests/test_mCSEATest.R
+++ inst/unitTests/test_mCSEATest.R
@@ -1,4 +1,4 @@
 test_mCSEATest <- function() {
-    checkEqualsNumeric(mCSEATest(myRank[1:100], "p", platform = "E")[[1]][1,1],
+    checkEqualsNumeric(mCSEATest(myRank[1:100], "p", platform = "E")[[1]]["PKN3","pval"],
                     0.08897591, tolerance=1.0e-6)
 }
