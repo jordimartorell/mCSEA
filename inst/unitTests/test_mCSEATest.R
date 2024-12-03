45dd75dc74ea1f4dbf5f799e1f9c0554039d4d7b
jordimartorell <jmartorellm@gmail.com>
2024-12-03 09:41:36 +0100

    Update myRank and test

--- inst/unitTests/test_mCSEATest.R
+++ inst/unitTests/test_mCSEATest.R
@@ -2,5 +2,5 @@ test_mCSEATest <- function() {
     set.seed(123)
     checkEqualsNumeric(mCSEATest(myRank[1:100], betaTest, phenoTest, 
                                     regionsTypes = "p", platform = "E")[["promoters"]]["PKN3","ES"],
-                    0.6900556, tolerance=1.0e-6)
+                    0.6470448, tolerance=1.0e-6)
 }
